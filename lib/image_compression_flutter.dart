bd090f7493da45a42ec7a5abbd8a406d14cec643
alann-maulana <alann_it06@yahoo.co.id>
2021-07-31 17:26:16 +0700

    update to support CrossFile

--- lib/image_compression_flutter.dart
+++ lib/image_compression_flutter.dart
@@ -3,6 +3,11 @@ import 'src/compressor.dart'
     if (dart.library.html) 'src/compressor_html.dart';
 import 'src/interface.dart';
 
+export 'package:cross_file/cross_file.dart'
+    show XFile;
+export 'package:image_compression/image_compression.dart'
+    show ImageFile;
 export 'src/configurations.dart';
+export 'src/cross_file_ext.dart';
 
 final ImageCompressionFlutter compressor = getCompressor();
