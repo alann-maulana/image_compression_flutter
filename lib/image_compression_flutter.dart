a2d4745648f62a0d489263d274dd55b571108e75
alann-maulana <alann_it06@yahoo.co.id>
2021-07-31 17:29:54 +0700

    reformat

--- lib/image_compression_flutter.dart
+++ lib/image_compression_flutter.dart
@@ -3,10 +3,9 @@ import 'src/compressor.dart'
     if (dart.library.html) 'src/compressor_html.dart';
 import 'src/interface.dart';
 
-export 'package:cross_file/cross_file.dart'
-    show XFile;
-export 'package:image_compression/image_compression.dart'
-    show ImageFile;
+export 'package:cross_file/cross_file.dart' show XFile;
+export 'package:image_compression/image_compression.dart' show ImageFile;
+
 export 'src/configurations.dart';
 export 'src/cross_file_ext.dart';
 
